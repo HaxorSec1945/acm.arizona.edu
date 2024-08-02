66fa8eb019ac2af9e24bc893f8176fe5b7311ae0
zerowork <dendisaimam5@gmail.com>
2024-08-02 09:09:44 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none
+<a href="https://siakad.uny.ac.id/sgacor/index.html">https://siakad.uny.ac.id/sgacor/index.html</a> 
 <a href="https://ternaktropika.ub.ac.id/lib/to/">https://ternaktropika.ub.ac.id/lib/to/</a> 
 <a href="https://ternaktropika.ub.ac.id/lib/thailand/">https://ternaktropika.ub.ac.id/lib/thailand/</a> 
 <a href="https://international.unsika.ac.id/style/pto/">https://international.unsika.ac.id/style/pto/</a> 
